0b80978e48a1e2081646ca63464d23d519ed8789
Jules <92145165+maisonnavejul@users.noreply.github.com>
2025-04-18 15:11:19 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -116,7 +116,7 @@ fi
 echo "Etape 6: Vérification des dépendances: (à implémenter...)"
 # Installer les dépendances Node.js
 #npm install express cors http socket.io os dockerode ldapjs
-sudo apt install ldap-utils
+sudo apt install -y ldap-utils
 # Vérifier le code de retour de npm install
 if [ $? -eq 0 ]; then
     echo ""
